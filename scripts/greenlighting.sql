631b10b0a1c244d85cbce4ab3032386a073d048a
SalmaAllam8 <saturnsailor8@gmail.com>
2026-06-16 19:34:16 +0300

    CTE

--- scripts/greenlighting.sql
+++ scripts/greenlighting.sql
@@ -11,3 +11,29 @@ having  max(estimated_revenue_million_usd) > (
 )
 
 order by Maximum_revenue desc  , number_of_games asc
+
+
+
+
+WITH genre_champions AS (
+    SELECT 
+        genre, 
+        MAX(estimated_revenue_million_usd) AS max_genre_revenue
+    FROM games
+    GROUP BY genre
+)
+
+
+SELECT 
+    g.title,
+    g.genre,
+    g.estimated_revenue_million_usd AS game_revenue,
+    c.max_genre_revenue AS champion_revenue,
+    
+   
+    ROUND(c.max_genre_revenue - g.estimated_revenue_million_usd, 2) AS gap_from_peak
+    
+FROM games g
+
+JOIN genre_champions c ON g.genre = c.genre
+ORDER BY g.genre ASC, gap_from_peak ASC;
