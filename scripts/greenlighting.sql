ce431aa86807f13861bf09a2e024c8419e4b8bb9
SalmaAllam8 <saturnsailor8@gmail.com>
2026-06-16 18:57:28 +0300

    updated

--- scripts/greenlighting.sql
+++ scripts/greenlighting.sql
@@ -1,6 +1,13 @@
 /*
 which genres are oversaturated and which ones have a high financial ceiling but low competition. */
 
-select genre , count(title) as number_of_games , max(estimated_revenue_million) as Maximum_revenue 
+select genre , count(title) as number_of_games , max(estimated_revenue_million_usd) as Maximum_revenue 
 
-from videogames.games
+from games
+
+group by genre 
+having  max(estimated_revenue_million_usd) > (
+    SELECT AVG(estimated_revenue_million_usd) FROM games
+)
+
+order by Maximum_revenue desc  , number_of_games asc
