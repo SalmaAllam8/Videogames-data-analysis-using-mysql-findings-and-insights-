f619f1f7ef868a49a5b15c5b3fb37cc05c0b01de
SalmaAllam8 <saturnsailor8@gmail.com>
2026-06-19 18:03:59 +0300

    top 3

--- scripts/greenlighting.sql
+++ scripts/greenlighting.sql
@@ -69,7 +69,41 @@ ORDER BY revenue_tier ASC;
 
 
 
+/*
+ "Top 3 Greenlight Recommendations" report. For each genre , the top 3 most profitable combinations
+  of distribution_model (Game Pass vs. Premium) and monetization strategy.
+ */
 
+WITH ranked_strategies AS (
+    SELECT 
+        genre,
+        CASE 
+            WHEN game_pass_available = 1 AND (microtransactions = 1 OR loot_boxes = 1) THEN 'Game Pass + Live-Service'
+            WHEN game_pass_available = 1 AND (microtransactions = 0 AND loot_boxes = 0) THEN 'Game Pass + Premium'
+            WHEN game_pass_available = 0 AND (microtransactions = 1 OR loot_boxes = 1) THEN 'Purchase-Only + Live-Service'
+            ELSE 'Traditional Purchase-Only'
+        END AS strategic_model,
+        ROUND(AVG(estimated_revenue_million_usd), 2) AS avg_revenue_million,
+        
+        
+        ROW_NUMBER() OVER (
+            PARTITION BY genre 
+            ORDER BY AVG(estimated_revenue_million_usd) DESC
+        ) AS strategy_rank
+
+    FROM games
+    GROUP BY genre, game_pass_available, microtransactions, loot_boxes
+)
+
+
+SELECT 
+    genre,
+    strategy_rank,
+    strategic_model,
+    avg_revenue_million
+FROM ranked_strategies
+WHERE strategy_rank <= 2
+ORDER BY genre ASC, strategy_rank ASC;
 
 
 
